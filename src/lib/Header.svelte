6456ca6ca922ffba01245a63627cd6e4496352d4
wyzwanie <shuib@pme.me>
2021-04-13 18:57:48 +0200

    myk i fixniete

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -21,11 +21,15 @@
     <div class="search">
         <input type="text" bind:value={searchTerm} placeholder="search" on:keyup={handleSearch}>
     </div>
-    <div class="mode">
+    <div class="theme">
         <Toggle on:status={e => dispatch('theme', e.detail)} />
     </div>
     <div class="settings">
-        notification/settings
+        <a href="/settings">
+            <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="42.163px" height="42.163px" viewBox="0 0 42.163 42.163" style="enable-background:new 0 0 32 32;" xml:space="preserve" fill="var(--accent)">
+                <g><path d="M37.364,15.082c-0.993,0-1.229-0.569-0.526-1.271l1.271-1.273c1.173-1.171,1.173-3.068,0-4.241l-4.241-4.242 c-1.172-1.172-3.07-1.173-4.243,0.001c0,0-0.568,0.569-1.271,1.271c-0.701,0.702-1.271,0.466-1.271-0.527V3 c0.001-1.657-1.34-2.997-2.998-3l-6.001,0.001c-1.659,0-3.002,1.343-3.002,3c0,0,0,0.805,0,1.797c0,0.991-0.569,1.226-1.271,0.525 l-1.27-1.27c-1.172-1.171-3.067-1.171-4.242-0.001L4.057,8.295c-1.171,1.173-1.173,3.069,0.001,4.245c0,0,0.569,0.569,1.271,1.272 c0.702,0.702,0.466,1.271-0.529,1.271H3c-1.657,0-3,1.343-3,3v6c0,1.657,1.343,3,3,3c0,0,0.806,0,1.799,0 c0.993,0,1.229,0.569,0.527,1.271l-1.271,1.271c-1.172,1.172-1.172,3.07,0,4.243l4.242,4.243c1.173,1.171,3.071,1.172,4.244,0 c0,0,0.569-0.568,1.27-1.271c0.701-0.701,1.271-0.466,1.271,0.526v1.796c0,1.451,1.029,2.66,2.397,2.939 c0.195,0.039,0.399,0.062,0.607,0.062h5.998c1.658-0.001,3-1.344,3.002-3.001c0,0,0-0.805,0-1.798s0.569-1.229,1.271-0.527 l1.271,1.271c1.173,1.174,3.071,1.174,4.245,0.002l4.241-4.242c1.172-1.172,1.172-3.072,0-4.245c0,0-0.568-0.567-1.271-1.271 c-0.701-0.701-0.467-1.271,0.524-1.271h1.795c1.657,0,3-1.343,3-3v-6c0-1.657-1.343-3-3-3 C39.162,15.082,38.357,15.082,37.364,15.082z M21.08,30.916c-5.432,0-9.835-4.402-9.835-9.834c0-5.432,4.403-9.835,9.835-9.835 c5.432,0,9.835,4.403,9.835,9.835C30.915,26.514,26.512,30.916,21.08,30.916z"/></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g>
+            </svg>
+        </a>
     </div>
 </header>
 <div class="sidebar">
@@ -38,60 +42,67 @@
 </div>
 
 <style>
-    .sidebar {
-        position: fixed;
-        left: 0;
-        top: 55px;
-        background: var(--bg-dark-second);
-        border-right: 1px solid rgba(255,255,255,0.05);
-        width: 96px;
-        height: 100%;
-    }
-    .menu {
-        display: flex;
-        list-style: none;
-        justify-content: center;
-        flex-flow: column;
-    }
-    .menu li {
-        padding: 9px;
-    }
-    header {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        border-bottom: 1px solid rgba(255,255,255,0.125);
-        background: var(--bg-dark-second);
-        padding: 9px;
-    }
-    .logo {
-        color: var(--accent);
-    }
-    .logo span {
-        font-weight: 100;
-        font-size: 90%;
-        color: white;
-    }
-    .active {
-        font-weight: bold;
-        color: var(--accent);
-    }
-    .logo {
-        font-weight: bold;
-    }
-    .search {
-        flex-grow: 0.618;
-    }
-    input {
-        width: 100%;
-        background: transparent;
-        color: var(--accent);
-        font-size: 1.2em;
-        border: none;
-        border-bottom: 1px solid white;
-    }
-    input:focus {
-        outline: none;
-        border-bottom: 2px solid var(--accent)
-    }
+.sidebar {
+    position: fixed;
+    left: 0;
+    top: 64px;
+    background: var(--bg-dark-second);
+    border-right: 1px solid rgba(255,255,255,0.05);
+    width: 96px;
+    height: calc(100vh - 64px);
+}
+.menu {
+    display: flex;
+    list-style: none;
+    justify-content: center;
+    flex-flow: column;
+}
+.menu li {
+    padding: 9px;
+}
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid rgba(255,255,255,0.125);
+    background: var(--bg-dark-second);
+    padding: 9px;
+}
+.logo {
+    color: var(--accent);
+}
+.logo span {
+    font-weight: 100;
+    font-size: 90%;
+    color: white;
+}
+.active {
+    font-weight: bold;
+    color: var(--accent);
+}
+.logo {
+    font-weight: bold;
+}
+.search {
+    flex-grow: 0.618;
+}
+input {
+    width: 100%;
+    background: transparent;
+    color: var(--accent);
+    font-size: 1.2em;
+    border: none;
+    border-bottom: 1px solid white;
+}
+input:focus {
+    outline: none;
+    border-bottom: 2px solid var(--accent)
+}
+.theme {
+    align-self: start;
+}
+svg {
+    width: 32px;
+    cursor: pointer;
+}
 </style>
--- src/routes/settings.svelte
+++ src/routes/settings.svelte
@@ -1,8 +1,7 @@
 
 <script>
+    import { chooseInstance } from '$lib/_helper'
     import { store, chosen } from '$lib/_store'
-    
-    let filteredInstances
 
     const saveSettings = () => {
         $store = $store
@@ -13,25 +12,27 @@
 		console.log(saveData)
     }
     $: console.log($store.instances)
+    $: console.log($chosen)
 </script>
 
-{#if $store && $store.instances && Object.keys($store.instances).length > 0}
-    <h1>Settings</h1>
-    <h3>Chose instances rotation:</h3>
-    <ul style="list-style: none;">
-        {#each Object.keys($store.instances) as uri, i}
-            <li>
-                <input type="checkbox" bind:checked={$store.instances[uri]}>
-                <span style="margin-right:7px"></span>
-                {uri}
-                <span style="margin-right:7px"></span>
-                {Object.values($store.instances)[i]}
-            </li>
-        {/each}
-    </ul>
-    <button on:click={saveSettings}>Save</button>
-    <button on:click={() => window.location = chosen}>Back To Invidious</button>
-{/if}
+<h1>Settings</h1>
+<h3>Chose instances rotation:</h3>
+    {#if $store && $store.instances && $store.instances.length > 0}
+        <ul style="list-style: none;">
+            {#each $store.instances as instance, index}
+                <li>
+                    <input type="checkbox" bind:checked={$store.instances[index].enabled}>
+                    <span style="margin-right:7px"></span>
+                    {instance[0]}
+                    <span style="margin-right:7px"></span>
+                    {instance[index].flag}
+                </li>
+            {/each}
+        </ul>
+    {/if}
+<button on:click={saveSettings}>Save</button>
+<button on:click={() => window.location = chosen}>Back To Invidious</button>
+
 
 <!-- fetch('https://yewtu.be/feed/private?token=RBugBT8LPiRzvjkyXm3NZ2MttL2mk23d_XE1tHQ7C4s=')
   .then(response => response.text())
