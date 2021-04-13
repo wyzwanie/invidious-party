1d41ea281c97d26a55bfc7bb4a7d6b3f79e33961
wyzwanie <shuib@pme.me>
2021-04-13 19:52:21 +0200

    refactoring, added RSS to settings

--- src/routes/settings.svelte
+++ src/routes/settings.svelte
@@ -1,40 +1,38 @@
 
 <script>
-    import { chooseInstance } from '$lib/_helper'
+    import { saveLocal } from '$lib/_helper'
     import { store, chosen } from '$lib/_store'
-
-    const saveSettings = () => {
-        $store = $store
-        const { instances } = $store
-        saveData.settings = $store.
-		saveData.time = new Date().getTime();
-		localStorage.saveData = JSON.stringify(saveData);
-		console.log(saveData)
-    }
-    $: console.log($store.instances)
-    $: console.log($chosen)
 </script>
 
 <h1>Settings</h1>
-<h3>Chose instances rotation:</h3>
-    {#if $store && $store.instances && $store.instances.length > 0}
-        <ul style="list-style: none;">
-            {#each $store.instances as instance, index}
-                <li>
-                    <input type="checkbox" bind:checked={$store.instances[index].enabled}>
-                    <span style="margin-right:7px"></span>
-                    {instance[0]}
-                    <span style="margin-right:7px"></span>
-                    {instance[index].flag}
-                </li>
-            {/each}
-        </ul>
-    {/if}
-<button on:click={saveSettings}>Save</button>
+<div class="wrapper">
+    <div class="left">
+        <h3>Chose instances rotation:</h3>
+        {#if $store && $store.instances && $store.instances.length > 0}
+            <ul style="list-style: none;">
+                {#each $store.instances as instance, index}
+                    <li>
+                        <input type="checkbox" bind:checked={$store.instances[index][1].enabled}>
+                        <span style="margin-right:7px"></span>
+                        {instance[0]}
+                        <span style="margin-right:7px"></span>
+                        {instance[1].flag}
+                    </li>
+                {/each}
+            </ul>
+        {/if}
+    </div>
+    <div class="right">
+        <h3>Input RSS for subscription:</h3>
+        <input type="text" bind:value={$store.rss}>
+    </div>    
+</div>
+
+<button on:click={() => saveLocal({ instances: $store.instances, rss: $store.rss })}>Save</button>
 <button on:click={() => window.location = chosen}>Back To Invidious</button>
 
 
-<!-- fetch('https://yewtu.be/feed/private?token=RBugBT8LPiRzvjkyXm3NZ2MttL2mk23d_XE1tHQ7C4s=')
+<!-- fetch('')
   .then(response => response.text())
   .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
   .then(data => {
